02cbbfb62e5fcc946b259142db7e1a2f4344c3d2
KarineDunberry <dunberryk@gmail.com>
2020-10-15 16:00:23 -0400

    modifier et supprimer une catégorie
    item relié à la catégorie

--- js/script.js
+++ js/script.js
@@ -20,13 +20,14 @@ function choisirCategorie() {
 function choisirItem() {
   let clone = $("#creer__item_template").clone();
   let input = clone.find(".item__input_nom");
+  let IDcategorie = $(this).parent().parent().attr("id");
   
   $("#show-choix").empty();
   $("#show-choix").append(clone);
   clone.show();
 
   clone.find(".btn-close-item").click(fermerFenetreChoix);
-  $(".btn__ajout_item").click(creerOngletItem);
+  creerOngletItem(IDcategorie);
 }
 
 function creerOngletCategorie() {
@@ -41,37 +42,53 @@ function creerOngletCategorie() {
 
   } else {
     $(this).popover("hide");
-    clone.attr("id", "");                       
-    clone.css("background-color", couleur);
+    clone.attr("id", titre);                       
+    clone.find(".onglet__cadre_categorie").css("background-color", couleur);
     champTitre.append(titre);
     $("#show-liste").append(clone);
     clone.show();
     $("#show-choix").empty();
   }
 
-  clone.find(".onglet__btn_item").click(choisirItem);
+  clone.find(".categorie__icone_item").click(choisirItem);
+  clone.find(".categorie__icone_supprimer").click(supprimerCategorie);
+  clone.find(".categorie__icone_modifier").click(modifierCategorie);
 }
 
-function creerOngletItem() {
-  let clone = $("#item__creer_template").clone();
-  let titre = $(this).parent().siblings(".titre").find(".item__input_nom").val(); 
-  let champTitre = clone.find(".item__titre");
+function creerOngletItem(IDcategorie) {
+  $(".btn__ajout_item").click(function() {
+    let clone = $("#item__creer_template").clone();
+    let titre = $(this).parent().siblings(".titre").find(".item__input_nom").val(); 
+    let champTitre = clone.find(".item__titre");
+    let categorie = $("#" + IDcategorie);
 
-  if (titre == "") {
-    $(this).popover("show");
-    $(this).popover("disable");
+    if (titre == "") {
+      $(this).popover("show");
+      $(this).popover("disable");
 
-  } else {
-    $(this).popover("hide");
-    clone.attr("id", "");                       
-    champTitre.append(titre);
-    $("#show-liste").append(clone); //verifier texte popover
-    clone.show();
-    $("#show-choix").empty();
-  }
+    } else {
+      $(this).popover("hide");
+      clone.attr("id", "");                       
+      champTitre.append(titre);
+      categorie.find(".onglet__show_item").append(clone); //verifier texte popover
+      clone.show();
+      $("#show-choix").empty();
+    }
 
-  $(".item__icone_supprimer").click(supprimerItem);
-  $(".item__icone_modifier").click(modifierItem);
+    $(".item__icone_supprimer").click(supprimerItem);
+    $(".item__icone_modifier").click(modifierItem);
+  })
+}
+
+function enregistrerCategorie() {
+  let nouveauTitre = $(this).parent().parent().find(".input-nom").val();
+  let categorieAModifier = $("#target");
+  let couleur = $(".btn-radio-couleur:checked").css("background-color");
+
+  categorieAModifier.find(".onglet__cadre_categorie").css("background-color", couleur);
+  categorieAModifier.find(".titre-categorie").text(nouveauTitre);
+  $("#show-choix").empty();
+  categorieAModifier.attr("id", "");
 }
 
 function enregistrerItem() {
@@ -87,11 +104,32 @@ function fermerFenetreChoix() {
   $("#show-choix").empty();
 }
 
+function modifierCategorie() {
+  let categorieAModifier = $(this).parent().parent(".onglet-template");
+  let couleurCourante = $(this).parent(".onglet__cadre_categorie").css("background-color"); //comment cocher la bonne case?
+  let titreCourant = $(this).siblings(".titre-categorie").text();
+  let clone = $("#creer-categorie-template").clone();
+  let input = clone.find(".input-nom");
+
+  categorieAModifier.attr("id", "target");
+
+  $("#show-choix").empty();
+  clone.find(".btn-creer-onglet").text("Enregistrer");
+ 
+  input.val(titreCourant);
+  $("#show-choix").append(clone);
+  clone.show();
+
+  clone.find("btn-close-categorie").click(fermerFenetreChoix);
+  $(".btn-creer-onglet").click(enregistrerCategorie);
+}
+
 function modifierItem() {
   let itemAModifier = $(this).parent().parent().parent();
   let titreCourant = $(this).parent().siblings(".item__titre").text();
   let clone = $("#creer__item_template").clone();
   let input = clone.find(".item__input_nom");
+  
   itemAModifier.attr("id", "target");
   
   $("#show-choix").empty();
@@ -102,6 +140,10 @@ function modifierItem() {
 
   clone.find(".btn-close-item").click(fermerFenetreChoix);
   $(".btn__ajout_item").click(enregistrerItem);
+}
+
+function supprimerCategorie() {
+  $(this).parent().parent().remove();
 }
 
 function supprimerItem() {
@@ -128,6 +170,8 @@ $(document).ready(function() {
   });
 
   $(".btn-ajout-categorie").click(choisirCategorie);
+
+  
 
  
 
